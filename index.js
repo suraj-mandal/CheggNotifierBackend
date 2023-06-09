36691d2843e376bec7bcaccb03cf752ad8968b33
Greg-GJE3 <suraj.mandal738@gmail.com>
2023-06-09 23:47:00 +0530

    Updated the listening port to 5000

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (_, res) => {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
